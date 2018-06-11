711fa036e1e4abee79c8ea0de82eded0276c17a4
SerenaFeng <feng.xiaowei@zte.com.cn>
2018-06-11 19:16:20 +0800

    add regenerate project tree and message promoting
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- cliff_generator/cli/generate.py
+++ cliff_generator/cli/generate.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from jinja2 import Environment, PackageLoader
 
@@ -15,6 +16,10 @@ class Generation(cli.Command):
         # parser.add_argument('-p', '--package',
         #                     default='test_cliff',
         #                     help='Package name, substitute \- with \_ if not provided')
+        parser.add_argument('-rm',
+                            action='store_true',
+                            default=False,
+                            help='whether to rm existed project dir or not')
         parser.add_argument('-o', '--output',
                             help='Directory to place generated project')
         return parser
@@ -31,43 +36,56 @@ class Generation(cli.Command):
         pkg_dir = os.path.join(pdir, package)
         cli_dir = os.path.join(pkg_dir, 'cli')
 
-        # try:
-        #     os.makedirs(cli_dir)
-        # except Exception as err:
-        #     raise Exception('Create dir [{}] failed, due to: '.format(dir, str(err)))
+        print('begin generate project tree {}'.format(cli_dir))
+        if os.path.exists(pdir) and parsed_args.rm:
+            try:
+                shutil.rmtree(pdir)
+            except Exception as err:
+                raise Exception('Delete project [{}] failed, due to {}'.format(pdir, err))
+
+        if not os.path.exists(cli_dir):
+            try:
+                os.makedirs(cli_dir)
+            except Exception as err:
+                raise Exception('Create dir [{}] failed, due to: {}'.format(dir, err))
 
         env = Environment(loader=PackageLoader('cliff_generator', 'templates'))
+
+        print 'begin to generate {}/setup.cfg'.format(pdir)
         setup_cfg_t = env.get_template('setup.cfg.j2')
         setup_cfg = setup_cfg_t.render(project=config.project)
         self.write_file('setup.cfg', setup_cfg, pdir)
 
+        print 'begin to generate {}/setup.py'.format(pdir)
         setup_py_t = env.get_template('setup.py.j2')
         setup_py = setup_py_t.render()
         self.write_file('setup.py', setup_py, pdir)
 
+        print 'begin to generate {}/shell.py'.format(pkg_dir)
         shell_py_t = env.get_template('shell.py.j2')
         shell_py = shell_py_t.render(project=config.project,
                                      cls=pname.replace('-', '').capitalize())
         self.write_file('shell.py', shell_py, pkg_dir)
 
+        print 'begin to generate {}/__init__.py'.format(pkg_dir)
         init_py_empty_t = env.get_template('__init__.py.empty.j2')
         init_py_empty = init_py_empty_t.render(author=config.project.get('author'))
         self.write_file('__init__.py', init_py_empty, pkg_dir)
 
+        print 'begin to generate {}/__init__.py'.format(cli_dir)
         init_py_t = env.get_template('__init__.py.j2')
         init_py = init_py_t.render()
         self.write_file('__init__.py', init_py, cli_dir)
 
         for sub, actions in config.project.get('subs').iteritems():
-            print 'begin to render sub {}'.format(sub)
+            print 'begin to generate {}/{}.py'.format(cli_dir, sub)
             sub_py_t = env.get_template('sub.py.j2')
             sub_py = sub_py_t.render(package=package, sub=sub, actions=actions)
             self.write_file('{}.py'.format(sub), sub_py, cli_dir)
 
-        return 'success'
+        return 'Congrats: Generate Success'
 
     def write_file(self, filename, content, dir):
         fdir = os.path.join(dir, filename)
-        print content
         with open(fdir, 'w') as fd:
             fd.write(content)
