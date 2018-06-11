5fdfc79c36697ec3e71bc601aa2394bb5462e220
SerenaFeng <feng.xiaowei@zte.com.cn>
2018-06-11 19:51:22 +0800

    leverage conf_file conf.yaml.sample for configration
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- cliff_generator/cli/generate.py
+++ cliff_generator/cli/generate.py
@@ -1,21 +1,17 @@
 import os
 import shutil
-
+import yaml
 from jinja2 import Environment, PackageLoader
 
 from cliff_generator import cli
-from cliff_generator import config
 
 
 class Generation(cli.Command):
     def get_parser(self, prog_name):
         parser = super(Generation, self).get_parser(prog_name)
-        # parser.add_argument('-n', '--name',
-        #                     default='test-cliff',
-        #                     help='Project name to be generated')
-        # parser.add_argument('-p', '--package',
-        #                     default='test_cliff',
-        #                     help='Package name, substitute \- with \_ if not provided')
+        parser.add_argument('conf_file',
+                            type=str,
+                            help='config file in yaml')
         parser.add_argument('-rm',
                             action='store_true',
                             default=False,
@@ -26,11 +22,19 @@ class Generation(cli.Command):
 
     def take_action(self, parsed_args):
 
-        pname = config.project.get('name')
-        package = config.project.get('package')
+        with open(parsed_args.conf_file, 'r') as cfd:
+            conf = yaml.safe_load(cfd)
+
+        pname = conf.get('project')
+        package = conf.get('package')
         if not package:
             package = pname.replace('-', '_')
-            config.project.update({'package': package})
+            conf.update({'package': package})
+        cliname = conf.get('cli')
+        if not cliname:
+            cliname = pname
+            conf.update({'cli': pname})
+
         root_dir = parsed_args.output if parsed_args.output else './'
         pdir = os.path.join(root_dir, pname)
         pkg_dir = os.path.join(pdir, package)
@@ -53,7 +57,7 @@ class Generation(cli.Command):
 
         print 'begin to generate {}/setup.cfg'.format(pdir)
         setup_cfg_t = env.get_template('setup.cfg.j2')
-        setup_cfg = setup_cfg_t.render(project=config.project)
+        setup_cfg = setup_cfg_t.render(conf=conf)
         self.write_file('setup.cfg', setup_cfg, pdir)
 
         print 'begin to generate {}/setup.py'.format(pdir)
@@ -63,13 +67,13 @@ class Generation(cli.Command):
 
         print 'begin to generate {}/shell.py'.format(pkg_dir)
         shell_py_t = env.get_template('shell.py.j2')
-        shell_py = shell_py_t.render(project=config.project,
+        shell_py = shell_py_t.render(project=pname,
                                      cls=pname.replace('-', '').capitalize())
         self.write_file('shell.py', shell_py, pkg_dir)
 
         print 'begin to generate {}/__init__.py'.format(pkg_dir)
         init_py_empty_t = env.get_template('__init__.py.empty.j2')
-        init_py_empty = init_py_empty_t.render(author=config.project.get('author'))
+        init_py_empty = init_py_empty_t.render(author=conf.get('author'))
         self.write_file('__init__.py', init_py_empty, pkg_dir)
 
         print 'begin to generate {}/__init__.py'.format(cli_dir)
@@ -77,7 +81,7 @@ class Generation(cli.Command):
         init_py = init_py_t.render()
         self.write_file('__init__.py', init_py, cli_dir)
 
-        for sub, actions in config.project.get('subs').iteritems():
+        for sub, actions in conf.get('subs').iteritems():
             print 'begin to generate {}/{}.py'.format(cli_dir, sub)
             sub_py_t = env.get_template('sub.py.j2')
             sub_py = sub_py_t.render(package=package, sub=sub, actions=actions)
