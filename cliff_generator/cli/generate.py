d4c44607eaae58674bfaa34decd9fa8956432df2
SerenaFeng <feng.xiaowei@zte.com.cn>
2018-06-12 16:21:32 +0800

    restruct templates
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- cliff_generator/cli/generate.py
+++ cliff_generator/cli/generate.py
@@ -23,79 +23,79 @@ class Generation(cli.Command):
     def take_action(self, parsed_args):
 
         with open(parsed_args.conf_file, 'r') as cfd:
-            conf = yaml.safe_load(cfd)
+            self.conf = yaml.safe_load(cfd)
 
-        pname = conf.get('project')
-        package = conf.get('package')
-        if not package:
-            package = pname.replace('-', '_')
-            conf.update({'package': package})
-        if not conf.get('cli'):
-            conf.update({'cli': pname})
+        pname = self.conf.get('project')
+        if not self.conf.get('package'):
+            self.conf.update({'package': pname.replace('-', '_')})
+        if not self.conf.get('cli'):
+            self.conf.update({'cli': pname})
+        if not self.conf.get('entry-cls'):
+            self.conf.update({
+                'entry-cls': pname.replace('-', '').capitalize()
+            })
 
-        root_dir = parsed_args.output if parsed_args.output else './'
-        pdir = os.path.join(root_dir, pname)
-        pkg_dir = os.path.join(pdir, package)
-        cli_dir = os.path.join(pkg_dir, 'cli')
-        utils_dir = os.path.join(pkg_dir, 'utils')
-
-        print('begin generate project tree {}'.format(pkg_dir))
-        if os.path.exists(pdir) and parsed_args.rm:
+        self.root_dir = parsed_args.output if parsed_args.output else './'
+        self.pdir = os.path.join(self.root_dir, pname)
+        self.env = Environment(loader=PackageLoader('cliff_generator',
+                                                    'templates'))
+        if os.path.exists(self.pdir) and parsed_args.rm:
             try:
-                shutil.rmtree(pdir)
+                shutil.rmtree(self.pdir)
             except Exception as err:
-                raise Exception('Delete project [{}] failed, due to {}'.format(pdir, err))
+                raise Exception('Delete project [{}] failed, '
+                                'due to {}'.format(self.pdir, err))
 
-        for dir in [cli_dir, utils_dir]:
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+        self.templates_dir = os.path.join(os.path.dirname(this_dir),
+                                          'templates/')
+
+        print 'begin to create project [{}] tree'.format(pname)
+        for root, dirs, files in os.walk(self.templates_dir):
+            sr_dir = root.replace(self.templates_dir, '')
+            dr_dir = sr_dir.format(**self.conf)
+            dir = os.path.join(self.pdir, dr_dir)
             if not os.path.exists(dir):
                 try:
                     os.makedirs(dir)
                 except Exception as err:
-                    raise Exception('Create dir [{}] failed, due to: {}'.format(dir, err))
-
-        env = Environment(loader=PackageLoader('cliff_generator', 'templates'))
-
-        for file in [('setup.py', pdir),
-                     ('command.py', utils_dir),
-                     ('gitignore', pdir, '.gitignore')]:
-            print 'begin to generate {}/{}.py'.format(file[1], file[0])
-            self.copyfile(*file)
-
-        print 'begin to generate {}/setup.cfg'.format(pdir)
-        setup_cfg_t = env.get_template('setup.cfg.j2')
-        setup_cfg = setup_cfg_t.render(conf=conf)
-        self.writefile('setup.cfg', setup_cfg, pdir)
-
-        print 'begin to generate {}/shell.py'.format(pkg_dir)
-        shell_py_t = env.get_template('shell.py.j2')
-        shell_py = shell_py_t.render(conf=conf,
-                                     cls=pname.replace('-', '').capitalize())
-        self.writefile('shell.py', shell_py, pkg_dir)
-
-        print 'begin to generate {}/__init__.py'.format(pkg_dir)
-        for dir in [cli_dir, utils_dir, pkg_dir]:
-            init_py_t = env.get_template('__init__.py.j2')
-            init_py = init_py_t.render(author=conf.get('author'))
-            self.writefile('__init__.py', init_py, dir)
-
-        for sub, actions in conf.get('subs').iteritems():
-            print 'begin to generate {}/{}.py'.format(cli_dir, sub)
-            sub_py_t = env.get_template('sub.py.j2')
-            sub_py = sub_py_t.render(package=package, sub=sub, actions=actions)
-            self.writefile('{}.py'.format(sub), sub_py, cli_dir)
+                    raise Exception('Create dir [{}] failed, '
+                                    'due to: {}'.format(dir, err))
+            for file in files:
+                if file == 'sub.py.j2':
+                    self.render_subs(sr_dir, dr_dir)
+                elif file.endswith('.j2'):
+                    self.renderfile(file, sr_dir, dr_dir)
+                elif not file.endswith('.pyc'):
+                    self.copyfile(file, sr_dir, dr_dir)
 
         return 'Congrats: Generate Success'
 
-    def writefile(self, filename, content, dir):
-        fdir = os.path.join(dir, filename)
+    def renderfile(self, filename, sr_dir, relative_dir):
+        print 'begin to generate {}'.format(os.path.join(relative_dir, filename))
+        template = self.env.get_template(os.path.join(sr_dir, filename))
+        content = template.render(conf=self.conf)
+        self.writefile(filename[:-3], content, relative_dir)
+
+    def render_subs(self, sr_dir, relative_dir):
+        for sub, actions in self.conf.get('subs').iteritems():
+            print 'begin to render {}.py'.format(os.path.join(relative_dir, sub))
+            template = self.env.get_template(os.path.join(sr_dir, 'sub.py.j2'))
+            content = template.render(package=self.conf.get('package'),
+                                      sub=sub,
+                                      actions=actions)
+            self.writefile('{}.py'.format(sub), content, relative_dir)
+
+    def writefile(self, filename, content, relative_dir):
+        fdir = os.path.join(self.pdir, relative_dir, filename)
         try:
             with open(fdir, 'w') as fd:
                 fd.write(content)
         except Exception as err:
             print 'write file {} failed with: {}'.format(filename, err)
 
-    def copyfile(self, filename, dst, dstname=None):
-        this_dir = os.path.dirname(os.path.abspath(__file__))
-        templates_dir = os.path.join(os.path.dirname(this_dir), 'templates')
-        shutil.copyfile(os.path.join(templates_dir, filename),
-                        os.path.join(dst, filename if not dstname else dstname))
+    def copyfile(self, filename, sr_dir, dr_dir):
+        print 'begin to copy {}'.format(os.path.join(dr_dir, filename))
+        src_dir = os.path.join(self.templates_dir, sr_dir, filename)
+        dst_dir = os.path.join(self.pdir, dr_dir, filename)
+        shutil.copyfile(src_dir, dst_dir)
--- study.py
+++ study.py
@@ -1,4 +1,14 @@
 import os
 
+this_dir = os.path.dirname(os.path.realpath(__file__))
+print this_dir
+templates_dir = os.path.join(this_dir, 'cliff_generator/templates/')
 
-print os.pardir
+print templates_dir
+
+pdir = '/Users/serena/github/test_dir'
+
+for root, dirs, files in os.walk(templates_dir):
+    relative_dir = root.replace(templates_dir, '')
+    print relative_dir
+    print root, dirs, files
